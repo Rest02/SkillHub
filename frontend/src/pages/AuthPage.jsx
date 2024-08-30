a42e0cd38460736d2d8b709f2f2fdbc727137c6f
Rest <code.rest.dev@gmail.com>
2024-08-30 14:31:14 -0400

    0.0.6 Creando validaciónes con yum en el FRONTEND

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import { useAuth } from "../context/AuthContext";
-import { Form, Formik } from "formik";
+import { Form, Formik, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function AuthPage() {
   const { registerUser } = useAuth();
+
+  // Esquema de validación utilizando Yup
+  const validationSchema = Yup.object({
+    nombre: Yup.string()
+      .min(3, "El nombre de usuario debe tener al menos 3 caracteres")
+      .max(8, "El nombre de usuario no debe superar los 8 caracteres")
+      .required("El nombre de usuario es obligatorio"),
+    email: Yup.string()
+      .email("Correo electrónico no válido")
+      .required("El correo electrónico es obligatorio"),
+    password: Yup.string()
+      .min(6, "La contraseña debe tener al menos 6 caracteres")
+      .required("La contraseña es obligatoria"),
+  });
 
   return (
     <div>
@@ -13,6 +28,7 @@ function AuthPage() {
           email: "",
           password: "",
         }}
+        validationSchema={validationSchema}
         onSubmit={async (values) => {
           console.log(values);
           await registerUser(values);
@@ -20,14 +36,23 @@ function AuthPage() {
       >
         {({ handleChange, handleSubmit }) => (
           <Form onSubmit={handleSubmit}>
-            <label>Nombre de usuario</label>
-            <input type="text" name="nombre" onChange={handleChange} />
+            <div>
+              <label>Nombre de usuario</label>
+              <Field type="text" name="nombre" onChange={handleChange} />
+              <ErrorMessage name="nombre" component="div" className="error" />
+            </div>
 
-            <label>Correo electrónico</label>
-            <input type="email" name="email" onChange={handleChange} />
+            <div>
+              <label>Correo electrónico</label>
+              <Field type="email" name="email" onChange={handleChange} />
+              <ErrorMessage name="email" component="div" className="error" />
+            </div>
 
-            <label>Contraseña</label>
-            <input type="password" name="password" onChange={handleChange} />
+            <div>
+              <label>Contraseña</label>
+              <Field type="password" name="password" onChange={handleChange} />
+              <ErrorMessage name="password" component="div" className="error" />
+            </div>
 
             <button type="submit">Guardar</button>
           </Form>
